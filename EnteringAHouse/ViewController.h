d8386047d963518491d47b82e9580645bb842681
Suvan <suvanramani@gmail.com>
2016-11-09 15:29:37 -0800

    Initial Commit

---
+++ EnteringAHouse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EnteringAHouse
+//
+//  Created by Suvan Ramani on 2016-11-09.
+//  Copyright © 2016 suvanr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ EnteringAHouse/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  EnteringAHouse
+//
+//  Created by Suvan Ramani on 2016-11-09.
+//  Copyright © 2016 suvanr. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
