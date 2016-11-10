6c91694ef19b99aeba21a4c7a23a3cdde36dca46
Suvan <suvanramani@gmail.com>
2016-11-09 16:50:02 -0800

    Sam ordered me to commit

---
+++ EnteringAHouse/CustomVC.h
@@ -0,0 +1,13 @@
+//
+//  CustomVC.h
+//  EnteringAHouse
+//
+//  Created by Suvan Ramani on 2016-11-09.
+//  Copyright © 2016 suvanr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomVC : UIViewController
+
+@end
---
+++ EnteringAHouse/CustomVC.m
@@ -0,0 +1,20 @@
+//
+//  CustomVC.m
+//  EnteringAHouse
+//
+//  Created by Suvan Ramani on 2016-11-09.
+//  Copyright © 2016 suvanr. All rights reserved.
+//
+
+#import "CustomVC.h"
+
+@implementation CustomVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+//    self.navigationItem.rightBarButtonItem
+    
+}
+
+@end
